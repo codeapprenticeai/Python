1a07a070138249bcab9ea6a82d0d27559331573b
Alex Young <Alex@alex-young.co.uk>
2023-04-28 11:16:34 +0100

    Final Challenges

--- Chapter 2 - Challenges.py
+++ Chapter 2 - Challenges.py
@@ -17,14 +17,27 @@ person_name = "Alice"
 print(person_name)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 2. Name Cases - print multiple names using uppercase,lowercase,titlecase
 
 # Code Apprentice's Attempt
 
 name = "Fred"
-print(name.lower) # Lower Case
-print(name.upper) # Upper Case
-print(name.title) # Title Case
+print(name.lower()) # Lower Case
+print(name.upper()) # Upper Case
+print(name.title()) # Title Case
 
 # ChatGPT Attempt (GPT3.5) - Weird it using lists, for doing this task, but this is what it's done (All valid ways of doing this)
 
@@ -75,5 +88,61 @@ print(name3.title())
 
 
 
+#3 Famous Quotes - find a quote and print that quote using quotation marks
+
+# Code Apprentice Attempt
+
+quote = 'Albert Einstein once said "A person who never made a mistake never tried anything new."'
+print(quote)
 
 
+# ChatGPT (3.5 Attempt)
+
+quote = "A person who never made a mistake never tried anything new. - Albert Einstein"
+print(f'"{quote}"')
+
+
+# ChatGPT (4 Attempt)
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
+
+
+
+
+
+
+
+
+
+
+
+
+# 4 Stripping Names - use a variable to represent a person's name, include some whitespace at the start and end and use the strip function to fix this and use a combination of \t \n
+
+# Code Apprentice Attempt
+
+firstname = "   Alex      "
+print(firstname)
+
+print(f"{firstname.lstrip()} \tThis is my left strip\n")
+print(f"{firstname.rstrip()} \tThis is my right strip\n")
+print(f"{firstname.strip()} \tThis is my full strip\n")
+
+
+# ChatGPT (3.5 Attempt)
+
+name = "\t  John Doe\n"
+
+# Strip whitespace, tabs, and newline characters from the name
+name_stripped = name.strip()
+
+# Print the stripped name
+print(f"Stripped name: '{name_stripped}'")
+
+
+# ChatGPT (4 Attempt)
+
+name_with_whitespace = "\t\n John Doe \n\t"
+stripped_name = name_with_whitespace.strip()
+
+print(f"Original name: '{name_with_whitespace}'")
+print(f"Stripped name: '{stripped_name}'")
