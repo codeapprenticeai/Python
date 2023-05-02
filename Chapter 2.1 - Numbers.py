3a7f4bb7134d5ee328e817ed403eeb17773b0186
Alex Young <Alex@alex-young.co.uk>
2023-05-02 12:19:31 +0100

    Last Part of Chapter 2

---
+++ Chapter 2.1 - Numbers.py
@@ -0,0 +1,17 @@
+# Integers
+
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(3 / 2)
+
+
+# Multiple Assignment
+
+x, y, z = 10,20,30
+print(x,y,z)
+
+
+# Constants
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
