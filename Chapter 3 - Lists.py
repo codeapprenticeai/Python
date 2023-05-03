8ce614566b46e4ca45934428704b94cc1560e363
Alex Young <Alex@alex-young.co.uk>
2023-05-03 14:58:15 +0100

    Chapter 3 Updated

--- Chapter 3 - Lists.py
+++ Chapter 3 - Lists.py
@@ -3,3 +3,16 @@
 bikes = ['Trek', 'Redline', 'Cannondale', 'Specialized']
 print(bikes)
 
+# Access items in that list
+
+print(bikes[1].title())
+
+# Accessing the last item in that list
+
+print(bikes[-1].title())
+
+# Using Individual values from the list
+
+message = f"My First bicycle was a {bikes[0].title()}."
+print(message)
+
