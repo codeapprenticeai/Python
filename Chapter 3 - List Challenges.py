97f70c63326d8b250b44846a3844f3081351d613
Alex Young <Alex@alex-young.co.uk>
2023-05-04 15:47:29 +0100

    List Challenges

---
+++ Chapter 3 - List Challenges.py
@@ -0,0 +1,19 @@
+# Guest List
+
+# Part 1 - Make a list that includes at least 3 people
+
+guest_list = []
+
+guest_list.append('Jim')
+guest_list.append('Ralph')
+guest_list.append('Mike')
+
+print(guest_list)
+
+
+# Little bit of cheating on this task, but i think i have learnt in my days of powershell if you know of a shorter way of doing this, DO IT!
+
+for guest in guest_list:
+    print(f"I would like to formally invite {guest} to my dinner party")
+
+
