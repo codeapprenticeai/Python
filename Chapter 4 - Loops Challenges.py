da557a7cd004f1895fc8e3a2e161f0eab7fd70e9
Alex Young <Alex@alex-young.co.uk>
2023-05-10 12:21:16 +0100

    Chapter 4

---
+++ Chapter 4 - Loops Challenges.py
@@ -0,0 +1,18 @@
+# counting to 20 using a for loop
+
+numbers = range(1,21)
+
+for number in numbers:
+    print(f"The number is: {number}")
+
+# count to a million
+one_million = range(1,1000001)
+
+for mil in one_million:
+   print(f"The number is: {mil}")
+
+# find the min,max and sum
+print(sum(one_million))
+print(min(one_million))
+print(max(one_million))
+
---
+++ Chapter 4 - Loops.py
@@ -0,0 +1,24 @@
+# First go at my for loop
+
+names = ['Alex', 'Jo', 'Luke', 'Lisa']
+for name in names:
+    print(name)
+    
+    
+# Pizza Challenge
+
+pizzas = ['Ham & Mushroom', 'Hot & Spicy','American Hot', 'Hawaiian Pizza']
+
+for pizza in pizzas:
+    print(f"\nI Like {pizza} it is so nice in my belly!!!")
+    
+print("\nThat concludes my Pizza journey")
+
+
+# range, all about the range
+
+numbers = list(range(1,6))
+print(numbers)
+
+print(min(numbers))
+print(max(numbers))
